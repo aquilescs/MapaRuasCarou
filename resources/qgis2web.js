e48c4f0623e74249077afb4a78acf0bff3af8d7e
aquilescs <43012139+aquilescs@users.noreply.github.com>
2025-04-28 20:53:51 -0300

    Update qgis2web.js
    mail-to Aquiles

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -702,12 +702,15 @@ map.on("rendercomplete", function(evt) {
         var attribution = document.getElementsByClassName('ol-attribution')[0];
         var attributionList = attribution.getElementsByTagName('ul')[0];
         var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
+        var quilcsAttribution = document.createElement('li');
+        quilcsAttribution.innerHTML = '<a href="mailto:aquiles.coelho@gmail.com">Mapa elaborado por Aquiles Silva • </a>';
         var qgis2webAttribution = document.createElement('li');
         qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot; ';
         var olAttribution = document.createElement('li');
         olAttribution.innerHTML = '<a href="https://openlayers.org/">OpenLayers</a> &middot; ';
         var qgisAttribution = document.createElement('li');
         qgisAttribution.innerHTML = '<a href="https://qgis.org/">QGIS</a>';
+        attributionList.insertBefore(quilcsAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
         attributionList.insertBefore(olAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgisAttribution, firstLayerAttribution);
