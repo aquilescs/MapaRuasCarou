9125cf2d47d67b21443f5fe6d0e4ff207e9e1fce
aquilescs <43012139+aquilescs@users.noreply.github.com>
2025-04-29 15:08:58 -0300

    Update ReuniesdeTrabalho_20_style.js
    color: red

--- styles/ReuniesdeTrabalho_20_style.js
+++ styles/ReuniesdeTrabalho_20_style.js
@@ -24,6 +24,7 @@ var style_ReuniesdeTrabalho_20 = function(feature, resolution){
         image: new ol.style.Icon({
                   imgSize: [580, 580],
                   scale: 0.05172413793103448,
+                  color: '#FF3131',
                   anchor: [15, 15],
                   anchorXUnits: "pixels",
                   anchorYUnits: "pixels",
